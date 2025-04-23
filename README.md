2126fe6241371be9ee58c93169cb9f6ddc45cf31
Joel Kagohara Sasaki <126785821+JoelKSasaki@users.noreply.github.com>
2025-04-23 17:48:58 +0000

    tarefa Cyrrus

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TarefaDiaria
+
+1) Armazenei as tarefas e subtarefas como objetos, que serão automaticamente criados com o status FALSE, pois uma tarefa que acabou de ser criada não tem como ser dada como concluída instantaneamente. As subtarefas são opcionais. Se existirem, todas terão que ser cumpridas para concluir a tarefa principal.
+
+2) Fiquei travado em relação a mostrar os elementos do(s) objeto(s) no HTML. Quando eu apertava o botão "Adicionar", os dados ficavam à mostra enquanto a página carregava e sumiam logo depois.
+
+3) Eu implementaria os filtros e eu faria os botões de apagar a tarefa e marcá-la como concluída (eu implementaria também marcar as subtarefas como concluída).
+
+4) Eu implementaria as funcionalidades que melhorariam sua experiência com a aplicação web.
+
+5) Usei a IA para sanar algumas dúvidas sobre a linguagem JavaScript em si, pois este é meu primeiro projeto que uso JavaScript de verdade.
--- tarefa.js
+++ tarefa.js
@@ -1,10 +1,10 @@
-document.getElementById('tarefa').addEventListener('submit', function(event){
-    event.preventDefault()
+document.addEventListener('DOMContentLoaded', function(){
 
     document.querySelector('#add').disabled = true
 
+    //se não tiver nada na caixa de texto, o botão estará desabilitado
     document.querySelector('#newtask').onkeyup = () => {
-        if (document.querySelector('#newtask').value == ''){
+        if ((document.querySelector('#newtask').value == '')){
             document.querySelector('#add').disabled = true
         } else {
             document.querySelector('#add').disabled = false
@@ -12,35 +12,59 @@ document.getElementById('tarefa').addEventListener('submit', function(event){
     }
 
     document.querySelector('#description').onkeyup = () => {
-        if (document.querySelector('#description').value == ''){
+        if ((document.querySelector('#description').value == '')){
             document.querySelector('#add').disabled = true
         } else {
             document.querySelector('#add').disabled = false
         }
     }
 
-    document.querySelector('#subtasks').onkeyup = () => {
-        if (document.querySelector('#subtasks').value == ''){
-            document.querySelector('#add').disabled = true
-        } else {
-            document.querySelector('#add').disabled = false
+    document.querySelector('form').onsubmit = () => {
+        //constantes que terão a nova tarefa, sua descrição e, se tiver, as suas subtarefas
+        const titulo = document.querySelector('#newtask').value
+        const descricao = document.querySelector('#description').value
+        const subtarefa = document.querySelector('#subtasks').value.split(', ').map(subtarefa => subtarefa.trim()).filter(s => s !== '');
+
+        //criação de objetos
+        const tarefa = {
+            title: titulo,
+            descricao: descricao,
+            status: false
         }
+
+        const subtarefas = {
+            subtarefa: subtarefa,
+            status: false
+        }
+
+        document.querySelector('#name').innerHTML = tarefa.title
+        document.querySelector('#dc').innerHTML = tarefa.descricao
+
+        localStorage.setItem('tarefa', JSON.stringify(tarefa)) 
+
+        //verificação de existência de subtarefas
+        if (subtarefa.length > 0 && subtarefa[0] !== ''){
+            for (i=0; i<subtarefa.length; i++){
+                const li = document.createElement('li')
+                li.innerHTML = subtarefa[i]
+                document.querySelector('#subtarefa').append(li)
+                localStorage.setItem('subtarefas', JSON.stringify(subtarefas)) 
+            } 
+        }
+
+        document.querySelector('#newtask').value = ''
+        document.querySelector('#description').value = ''
+        document.querySelector('#subtasks').value = ''
+        document.querySelector('#add').disabled = true
+
+        /*const t = localStorage.getItem('tarefa', JSON.parse(tarefa))
+        if (t){
+            document.querySelector('#name').innerHTML = `<div class="title" id="titulo">
+            <p class="nome" id="name">${task.title}</p>
+            </div>`
+            document.querySelector('#dc').innerHTML = `<div class="description" id="desc">
+            <p class="dc" id="dc">${task.descricao}</p>
+            </div>`
+        }*/
     }
-
-    const titulo = document.querySelector('#newtask').value
-    const descricao = document.querySelector('#newtask').value
-    const subtarefas = document.querySelector('#newtask').value.split(',').map(subtarefa => subtarefa.trim())
-
-    const tarefa = {
-        title: titulo,
-        descricao: descricao,
-        subtarefas: subtarefas,
-        status: false
-    }
-
-    document.querySelector('#titulo').innerHTML = titulo
-
-
-    localStorage.setItem('tarefa', JSON.stringify(tarefa))
-    
 })
